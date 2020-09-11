86c47b53284fff82d9ffc0bc73bde5620945742a
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-11 16:45:03 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -23,8 +23,8 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+  shipping_mainfest  
 
 end
 
